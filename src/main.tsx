f0c5010a0dd285c8fc0b665a0a040f337b0a24b4
Brian <brianc.contacto@gmail.com>
2023-01-20 21:21:33 -0300

    ci: fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
-import {
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import Header from '@components/header';
-import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Create a client
 const queryClient = new QueryClient();
